cccdbfc84410922e8e7b619a415c101dc536e057
tgaertner <tgaertner@abacus-edv.de>
2023-10-04 17:34:13 +0200

    Add kinds of missing values in gen_drop_out
    * Add parameters 'kind' and 'mnar_weight_column' to function
    * Change default function params of 'fraction' and 'vacation' to 0
    * Add different kinds to function

--- simulation.py
+++ simulation.py
@@ -44,6 +44,16 @@ def simulate_outcome(outcome_params, data, length, dependencies, causal_effects,
     return baseline_drift, underlying_state, observation
 
 
+def normalize(data):
+    """
+    Normalize Data
+    :param data:
+    :return:
+    """
+    data = np.array(data)
+    return (data - data.mean())/data.std()
+
+
 def simulate_treatment(study_design, days_per_period, treatment, dependencies, params, causal_effects, data):
     """
     Simulates a treatment with given parameters
@@ -67,11 +77,11 @@ def simulate_treatment(study_design, days_per_period, treatment, dependencies, p
 
     # Add dependencies
     for dependency in dependencies:
-        treatment_arr += np.array(data[dependency]) * causal_effects[
+        treatment_arr += normalize(data[dependency]) * causal_effects[
             "{} -> {}".format(dependency, treatment)]
     
     # Clip treatment to 1 or 0 
-    treatment_arr = np.where(np.array(treatment_arr)>= 0.5, 1, 0)
+    treatment_arr = np.where(np.array(treatment_arr) >= 0.5, 1, 0)
 
     # Get Params
     gamma = params["gamma"]
@@ -192,13 +202,13 @@ def gen_distribution(distribution, boarders=(0, 1), **params):
     :param params:
     :return:
     """
-    if distribution == "normal":
+    if distribution.lower() == "normal":
         val = gen_normal_distribution(**params)
-    elif distribution == "flag":
+    elif distribution.lower() == "flag":
         val = gen_flag(**params)
-    elif distribution == "poisson":
+    elif distribution.lower() == "poisson":
         val = gen_poisson_distribution(**params)
-    elif distribution == "unit":
+    elif distribution.lower() == "unit":
         val = gen_unit_distribution(boarders[0], boarders[1])
     else:
         val = None
@@ -243,37 +253,62 @@ def simulate_node(node, dependencies, length, data, params, causal_effects, over
     return result
 
 
-def gen_drop_out(data, fraction = None, vacation = None, drop_columns = None):
-    """
-    This function generates a random drop out.
-    :param data: pandas df with patient data
-    :param vacation: number of vacation days
-    :param fraction: fraction of sampling
-    :return: pandas data frame
-    """
+def gen_drop_out(data, fraction = 1, vacation = 0, drop_columns = None, kind="MAR", mnar_weight_column=None):
+    """This function generates a random drop out by using pandas.DataFrame.sample(). The weights for MAR were calculated by the time, for MNAR were given through the mnar_weight_column.
 
+    Args:
+        data (pandas df): pandas df with patient data
+        fraction (float, optional): fraction of sampling. Defaults to None.
+        vacation (int, optional): number of vacation days. Defaults to 0.
+        drop_columns (list, optional): list of columns, which are deleted for missing values. Defaults to None.
+        kind (str, optional): Givin the kind of missingness. Defaults to "MAR".
+        mnar_weight_column (str, optional): . Defauls to None
+
+    Raises:
+        ValueError: If kind of missing value is not defined.
+
+    Returns:
+        pd-DataFrame: Dataframe with missing values.
+    """
+    # create keep and drop columns
     if not drop_columns:
         keep_columns = ["patient_id","date","day","Treatment_1","Treatment_2"]
         drop_columns = list(set(list(data.columns))-set(keep_columns))
+
+    # assert that drop columns is greater 0
+    assert(len(drop_columns)>0)
 
     treatment_data = data.copy().drop(columns = drop_columns)
 
     # Apply vacation:
     # Continues period without any data
-    if vacation:
+    if vacation > 0:
         start = np.random.randint(1,len(data)-vacation)
         data = pd.concat([data[:start], data[start+vacation:]])
 
     # Drop out:
     # random drop out of data 
-    if fraction:
-        weights_drop_out = 1/(np.array(list(range(len(data))))+1)**2
-        weights_drop_out = 1/(np.array(list(range(len(data))))+1)
-        data = data.sample(frac = fraction, weights = weights_drop_out)
+    if fraction<=1:
+        if kind.lower() == "mcar":
+            # Function for "missing completly at random"
+            data = data.sample(frac = fraction)
+        elif kind.lower() == "mar":
+            # function for at "missing at random", increase over time
+            weights_drop_out = 1/(np.array(list(range(len(data))))+1)
+            data = data.sample(frac = fraction, weights = weights_drop_out)
+        elif kind.lower() == "mnar":
+            # function for "missing not at random"
+            weights_drop_out = data[mnar_weight_column]
+            data = data.sample(frac = fraction, weights = weights_drop_out)
+        else: 
+            raise ValueError("Must be MCAR, MAR, or MNAR! Found: {kind.upper()}")
 
-    # ToDo: Others?
-    data = treatment_data.join(data[drop_columns])
-    return data.sort_index()
+    # Join baseline variables with sampled data
+    result_data = treatment_data.join(data[drop_columns])
+    # Create col missing
+    result_data["missing"] = result_data[drop_columns[0]].isna()
+    # Return df
+    return result_data.sort_index()
 
 
 class Simulation:
@@ -288,7 +323,7 @@ class Simulation:
 
         self.variables = parameter["variables"]
         self.dependencies = extract_dependencies(
-            [*self.variables.keys(), *self.exposures_params, self.outcome_params["name"]], parameter["dependencies"])
+            [*self.variables.keys(), *self.exposures_params.keys(), self.outcome_params["name"]], parameter["dependencies"])
         self.effect_sizes = parameter["dependencies"]
         if "over_time_dependencies" in parameter.keys():
             self.over_time_dependencies = parameter["over_time_dependencies"]
@@ -337,7 +372,6 @@ class Simulation:
         result = {'patient_id': [patient_id]*length,
                   'date': dti,
                   'block': block,
-                  'treatment': treatment,
                   'day': list(range(1,length+1))}
 
         for node in ordered_node:
@@ -366,9 +400,11 @@ class Simulation:
             # if node is variable
             else:
                 result[node] = simulate_node(node, self.dependencies[node], length, result, params=self.variables[node],
-                                             causal_effects=self.effect_sizes,over_time_effects = self.over_time_dependencies[node])
+                                             causal_effects=self.effect_sizes,
+                                             over_time_effects=self.over_time_dependencies[node])
 
         data = pd.DataFrame(result)
+        data['treatment'] = data[list(self.exposures_params.keys())].idxmax(axis=1)
 
         if drop_out:
             return data, gen_drop_out(data.copy(), **drop_out)
